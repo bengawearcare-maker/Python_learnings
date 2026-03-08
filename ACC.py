46b8fd77b5c9e5dbd2a14bbb5684fc804bf63571
bengawearcare <bengawearcare@gmail.com>
2026-03-08 16:45:09 +0530

    second

--- ACC.py
+++ ACC.py
@@ -1,3 +1,5 @@
 ECU_name = "ACC"
 Network_name = "ACC_Vehicle_Network"
 Baud_rate = 250
+print (f'ECU name is {ECU_name} and connected with {Network_name} at {Baud_rate} kb/s') # To print formated string
+
