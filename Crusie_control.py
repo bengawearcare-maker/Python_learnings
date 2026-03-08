23e539f5a0ae2d4e38f918563872cf60209e6d21
bengawearcare <bengawearcare@gmail.com>
2026-03-08 17:34:40 +0530

    after first 2 classes

--- Crusie_control.py
+++ Crusie_control.py
@@ -1,9 +1,73 @@
-cruise_speed = 80
-print ("cruise speed is", cruise_speed)
+import math
+# cruise_speed = 80
+# print ("cruise speed is", cruise_speed)
 
-name = input("What is your name? ")
-print("Hello", name, "welcome to python learnings")
+# name = input("What is your name? ")
+# print("Hello", name, "welcome to python learnings")
 
-age = input("What is your age? ")
-print("your age is" , age, "years old")
-print (type(age))
+# age = input("What is your age? ")
+# print("your age is" , age, "years old")
+# print (type(age))
+
+# Default = 31
+# if age > str(Default):
+#     print("you are eligible for driving")
+# else:    
+#     print("you are not eligible for driving")
+
+print (5+3)
+print("Hi" + " "+ "Karamath")
+
+for i in range(5):
+    if i == 0:
+        print("[0] is 0")
+    else:
+        print("not 0")
+
+count = 0
+while count < 5:
+    print(count)
+    count += 2
+
+def add():
+    print  ("This is add function")
+    print(math.sqrt(2))
+
+add()
+
+
+path = "C:/Python/ACC.py"
+with open(path, "r") as f:
+    data = f.read()
+    print(data)
+
+class Dog: 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("Woof! Woof!")
+my_dog = Dog("Buddy", 3)
+my_dog.bark()
+
+url = "https://github.com/bengawearcare-maker/Python_learnings/commit/82040d45a4264143e4abb628521b414ef9d63e10"
+list_names = ["Karamath","sheik","mohammad"]
+dic_name = {"name1":"Karamath","name2":"sheik","name3":"mohammad"}
+tuple_name = ("karamath","sheik","mohammad")
+print(list_names[1])
+print(dic_name["name2"])
+print(tuple_name[1])
+if list_names[1] == dic_name["name2"] == tuple_name[1]:
+    print("both are same")
+else:
+    print("both are different")
+
+t_url = url.split("/")[-4]
+print(t_url)
+
+
+
+with open(path,"r") as f:
+    data = f.read()
+    print(data)
