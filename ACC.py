a53c91be7bedd916d4c30f771e7decacec7ac5cf
bengawearcare <bengawearcare@gmail.com>
2026-03-08 16:33:00 +0530

    simple

---
+++ ACC.py
@@ -0,0 +1,3 @@
+ECU_name = "ACC"
+Network_name = "ACC_Vehicle_Network"
+Baud_rate = 250
--- Crusie_control.py
+++ Crusie_control.py
@@ -1,2 +1,9 @@
 cruise_speed = 80
 print ("cruise speed is", cruise_speed)
+
+name = input("What is your name? ")
+print("Hello", name, "welcome to python learnings")
+
+age = input("What is your age? ")
+print("your age is" , age, "years old")
+print (type(age))
